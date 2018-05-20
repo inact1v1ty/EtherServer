3979319b1563326f0e1f6b356b9b141591ae53da
Alexander Voronkov <myaddictionisguitars@gmail.com>
2018-05-20 05:10:33 +0300

    fully working inventory!

--- EtherServer/Game/Inventory.cs
+++ EtherServer/Game/Inventory.cs
@@ -12,17 +12,14 @@ namespace EtherServer.Game
         public List<Artifact> artifacts;
 
         public async Task<int> getInventory(Player player){
-            Console.WriteLine("started bc connection");
+            Console.WriteLine("started blockchain connection");
             string playerAddress = "0x6b2fb2df3ae3b22b8a4842461231d7b0e556ffd3"; // player.address
             string contractAddress = "0xaafa19d6f354eee368e0bc6ed0a418cc8bf49763";
             string abi = ABI.artifactsAbi;
-
             // server account
-            var privateKey = "25b53b659d2f2dbd91c61848381ff73898b94d67f4b7c395b4dc98f0c7ce8fd9";
+            var privateKey = "d1b10e2390ffd68062732bf5444494539abfabf4bd7442e753b7d4d7adb67256";
             var account = new Nethereum.Web3.Accounts.Account(privateKey);
-
             Web3 web3 = new Web3(account);
-            Console.WriteLine("created web3 obj");
             Contract contract = web3.Eth.GetContract(abi, contractAddress);
             Console.WriteLine("got contract");
             var getItemsAmount = contract.GetFunction("getArtifactsAmount");
@@ -30,7 +27,6 @@ namespace EtherServer.Game
 
             var getArtifactOwner = contract.GetFunction("artifactIndexToOwner");
             var getArtifact = contract.GetFunction("getArtifactTypeId");
-            Console.WriteLine("started loop");
             for(int i = 0; i < amount; ++i){
                 var artifactOwner = await getArtifactOwner.CallAsync<string>(i);
                 if(artifactOwner == playerAddress){
